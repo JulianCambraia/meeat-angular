9c16f65104a335cca9a774c8c42d36d99e6a83d4
JulianFernando <juliancambraia@gmail.com>
2019-07-27 14:04:30 -0300

    Login e Logout Manualmente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { RestaurantComponent } from './restaurants/restaurant/restaurant.compone
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { SharedModule } from './shared/shared.module';
 import { LoginComponent } from './security/login/login.component';
+import { UserDetailComponent } from './header/user-detail/user-detail.component';
 
 
 
@@ -38,6 +39,7 @@ import { LoginComponent } from './security/login/login.component';
         OrderSummaryComponent,
         NotFoundComponent,
         LoginComponent,
+        UserDetailComponent,
     ],
     imports: [
         BrowserModule,
--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -1,17 +1,25 @@
+import 'rxjs/add/operator/filter';
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { MEAT_API } from 'app/app.api';
 import { Observable } from 'rxjs';
 
 import { Usuario } from './usuario.model';
-import { Router } from '@angular/router';
 
 @Injectable()
 export class LoginService {
 
     usuario: Usuario;
 
-    constructor(private httpClient: HttpClient, private router: Router) { }
+    lastUrl: string;
+
+    constructor(private httpClient: HttpClient, private router: Router) {
+        // registrando qual a ultima url clicada
+        this.router.events.filter(e => e instanceof NavigationEnd)
+            .subscribe((e: NavigationEnd) => this.lastUrl = e.url);
+    }
 
     isLoggedIn(): boolean {
         return this.usuario !== undefined;
@@ -22,7 +30,11 @@ export class LoginService {
             .do(usuario => this.usuario = usuario);
     }
 
-    handleLogin(path?: string) {
+    logout() {
+        this.usuario = undefined;
+    }
+
+    handleLogin(path: string = this.lastUrl) {
         this.router.navigate(['/login', btoa(path)]);
     }
 }
